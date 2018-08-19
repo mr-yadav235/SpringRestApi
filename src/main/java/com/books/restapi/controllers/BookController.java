bfcd5fd360f8559c71b9c0e8c537876232877fbb
mr-yadav235 <mr.yadav235@gmail.com>
2018-08-19 15:56:41 +0530

    put method added

--- src/main/java/com/books/restapi/controllers/BookController.java
+++ src/main/java/com/books/restapi/controllers/BookController.java
@@ -6,6 +6,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
@@ -31,6 +32,34 @@ public class BookController {
         return bookRepository.findOneById(id);
     }
 
+    @RequestMapping(value = "/books/{id}", method = RequestMethod.PUT)
+    public Book editById(@PathVariable("id") String id, @Valid @RequestBody Book book){
+        Book books = bookRepository.findOneById(id);
+        if(books.getTitle() != null)
+            books.setTitle(book.getTitle());
+        if(books.getDescription() != null)
+            books.setDescription(book.getDescription());
+        if(books.getPages() != 0)
+            books.setPages(book.getPages());
+        if(books.getBuy_url() != null)
+            books.setBuy_url(book.getBuy_url());
+        if(books.getIsbn() != null)
+            books.setIsbn(book.getIsbn());
+        if(books.getReviews() != null)
+            books.setReviews(book.getReviews());
+        if(books.getGenre() != null)
+            books.setGenre(book.getGenre());
+        if(books.getPublisher() != null)
+            books.setPublisher(book.getPublisher());
+        if(books.getAuthor() != null)
+            books.setAuthor(book.getAuthor());
+        if(books.getReleaseDate() != null)
+            books.setReleaseDate(book.getReleaseDate());
+
+        return books;
+
+    }
+
     @RequestMapping(method=RequestMethod.DELETE, value="/books/{id}")
     public String delete(@PathVariable String id) {
         Book book = bookRepository.findOneById(id);
@@ -38,4 +67,5 @@ public class BookController {
 
         return "product deleted";
     }
+
 }
